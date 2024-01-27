c12f0910be929b6d04b22eb5e1d90dda4595aa60
Amir <amahadnezhad@gmail.com>
2024-01-27 16:59:08 +0330

    Exercise3 -> Compare vs Assign

--- 02.py
+++ 02.py
@@ -2,6 +2,7 @@
 Exercise 2
 Question: What's wrong with the following script?
 """
+
 a = 1
 _a = 2
 _a2 = 3
---
+++ 03.py
@@ -0,0 +1,15 @@
+"""
+Exercise 3
+Question: Executing the code will throw an error. Can you explain why?
+"""
+
+a = 1
+b = 2
+print(a == b)
+print(b == c)
+
+# Hint: Operator == is a comparison operator.
+
+"""
+    Answer -> It's Throwing Error Because 'C' Variable is Not Defined
+"""
