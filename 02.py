8094d7776ca4838f14667129b2e95828408cbbcc
Amir <amahadnezhad@gmail.com>
2024-01-27 16:53:57 +0330

    Exercise2 -> Naming Rules

---
+++ 02.py
@@ -0,0 +1,13 @@
+"""
+Exercise 2
+Question: What's wrong with the following script?
+"""
+a = 1
+_a = 2
+_a2 = 3
+# 2a = 4
+
+"""
+Answer: Line 8 throws a SyntaxError because variables cannot start with a number.
+        Variable names must start with a letter or an underscore. Everything else will throw a SyntaxError.
+"""
