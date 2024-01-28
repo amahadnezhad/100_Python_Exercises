01ba7a720036d1fc02c432ac2165172bea980d75
Amir <amahadnezhad@gmail.com>
2024-01-28 16:43:25 +0330

    Exercise15 -> Simple Dictionary

---
+++ 15.py
@@ -0,0 +1,12 @@
+"""
+Exercise 15
+Question: Create a dictionary that contains the keys a and b and their respective values 1 and 2.
+
+"""
+
+# Answer
+x = {
+    'a':1,
+    'b':2,
+}
+print(type(x))
