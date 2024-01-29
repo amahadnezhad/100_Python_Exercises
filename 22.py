7f46d4faa424b857dfe8fefc6b513438bdf089db
Amir <amahadnezhad@gmail.com>
2024-01-29 14:46:53 +0330

    Exercise22 -> Formatted Print

---
+++ 22.py
@@ -0,0 +1,13 @@
+"""
+Exercise 22
+Question: Create a dictionary of keys a, b, c where each key has as value a list from 1 to 10, 11 to 20, and 21 to 30,
+          respectively. Then print out the dictionary in a nice format.
+
+Expected output:
+    {'a': 1}
+"""
+
+from pprint import pprint
+
+d = {"a":list(range(1, 11)), "b":list(range(11, 21)), "c":list(range(21, 31))}
+pprint(d)
