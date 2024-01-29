beb01dc1fec4c69ff88ae70b63d37e1a1d7562f7
Amir <amahadnezhad@gmail.com>
2024-01-29 16:26:21 +0330

    Exercise25 -> Print Alphabet

---
+++ 25.py
@@ -0,0 +1,17 @@
+"""
+Exercise 24
+Question: Make a script that prints out letters of the English alphabet from a to z, one letter per line in the terminal.
+
+Expected output:
+a
+b
+c
+.
+.
+z
+"""
+# Answer
+import string
+
+for letter in string.ascii_lowercase:
+    print(letter)
