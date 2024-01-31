caaac2001a6632f6f6d0f23fee1f94b99ac34bac
Amir <amahadnezhad@gmail.com>
2024-01-31 16:56:38 +0330

    Exercise32 -> Global Variable

---
+++ 32.py
@@ -0,0 +1,20 @@
+"""
+Exercise 32
+Question:  What will the following script output? Please try to do this by mind if you can.
+
+c = 1
+def foo():
+    return c
+c = 3
+print(foo())
+
+"""
+
+c = 1
+def foo():
+    return c
+c = 3
+print(foo())
+
+# Answer
+3
