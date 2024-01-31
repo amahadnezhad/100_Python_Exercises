0b47a617562b38fc4782fc3c7b38686895eb20f8
Amir <amahadnezhad@gmail.com>
2024-01-31 20:21:02 +0330

    Exercise37 -> nameError

---
+++ 37.py
@@ -0,0 +1,11 @@
+"""
+Exercise 37
+Question: The following code is supposed to print out the square root of 9,
+          but it throws an error instead because another line before that is missing.
+          Please fix the script so that it prints out the square root of 9.
+
+        math.sqrt(9)
+"""
+import math
+x = math.sqrt(9)
+print(x)
