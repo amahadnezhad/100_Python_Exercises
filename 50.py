11c5080c00bcd8aa9ee474bca5ebef024fb62c2f
Amir <amahadnezhad@gmail.com>
2024-02-03 18:05:25 +0330

    Exercise50 -> Text Input

---
+++ 50.py
@@ -0,0 +1,13 @@
+"""
+Exercise 50
+Question: The code produces an error. Please understand the error and try to fix it
+
+age = input("What's your age? ")
+age_last_year = age - 1
+print("Last year you were %s." % age_last_year)
+
+"""
+# Answer
+age = int(input("What's your age? "))
+age_last_year = age - 1
+print(f"Last year you were {age_last_year}")
