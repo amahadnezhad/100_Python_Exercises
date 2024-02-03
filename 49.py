19e4fb67cf5db3c34a148cc16eb774faa5281029
Amir <amahadnezhad@gmail.com>
2024-02-03 18:03:02 +0330

    Exercise49 -> Pass

---
+++ 49.py
@@ -0,0 +1,11 @@
+"""
+Exercise 49
+Question: The code is supposed to get some input from the user, but instead, it produces an error.
+          Please try to understand the error and then fix it.
+
+pass = input("Please enter your password: ")
+
+"""
+# Answer
+# pass = input("Please enter your password: ") -> SyntaxError
+pass1 = input("Please enter your password: ")
