75c78bc10cad2bf9df86b28141afd75cdbeed3a7
Amir <amahadnezhad@gmail.com>
2024-02-03 17:57:43 +0330

    Exercise47 -> Conditioned Letter Extractor

---
+++ 47.py
@@ -0,0 +1,20 @@
+"""
+Exercise 46
+Question: Create a script that iterates through text files and checks if strings p, y, t, h, o, or n are found
+          in the text file's content. If any of those strings is found, append that string to a list.
+"""
+
+# Answer
+import glob
+
+letters = []
+file_list = glob.iglob("Files/letters/*.txt")
+check = "python"
+
+for filename in file_list:
+    with open(filename, "r") as file:
+        letter = file.read().strip('\n')
+    if letter in check:
+        letters.append(letter)
+
+print(letters)
