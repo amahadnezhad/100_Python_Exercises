2c6d894f07b3fa553455b6940503c589f4b04c6f
Amir <amahadnezhad@gmail.com>
2024-02-04 18:59:38 +0330

    Exercise53 -> Multilevel Dictionary

---
+++ 53.py
@@ -0,0 +1,14 @@
+"""
+Exercise 53
+Question: Print out the last name of the second employee.
+
+Expected output:    Smith
+"""
+d = {"employees":[{"firstName": "John", "lastName": "Doe"},
+                {"firstName": "Anna", "lastName": "Smith"},
+                {"firstName": "Peter", "lastName": "Jones"}],
+"owners":[{"firstName": "Jack", "lastName": "Petter"},
+          {"firstName": "Jessy", "lastName": "Petter"}]}
+
+# Answer
+print(d['employees'][1]['lastName'])
