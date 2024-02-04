42ab20b4577facbcc75fcfdc4d64021b68045610
Amir <amahadnezhad@gmail.com>
2024-02-04 18:54:26 +0330

    Exercise51 -> EOF

---
+++ 51.py
@@ -0,0 +1,9 @@
+"""
+Exercise 51
+Question: The code produces an error. Please understand the error and try to fix it.
+
+"""
+# print(type("Hey".replace("ey","i")[-1]) # Syntax Error: '(' Was Never Closed.
+
+# Answer
+print(type("Hey".replace("ey","i")[-1]))
