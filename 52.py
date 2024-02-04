7164fd7e8ffd1dd5afa9b34f2a1077e4b168c679
Amir <amahadnezhad@gmail.com>
2024-02-04 18:57:23 +0330

    Exercise52 -> String Formatting

---
+++ 52.py
@@ -0,0 +1,13 @@
+"""
+Exercise 52
+Question: The code is supposed to ask the user to enter their name and surname,
+          and then it prints out those user submitted values.
+          Instead, the code throws a TypeError. Please fix it so that the expected output is printed out.
+
+"""
+first_name = input("Enter first name: ")
+second_name = input("Enter second name: ")
+
+print("Your first name is %s and your second name is %s" % (first_name, second_name))
+
+
