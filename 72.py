d39c4de474e8c5ab1112c66b439f999269c7885b
Amir <amahadnezhad@gmail.com>
2024-02-06 16:43:10 +0330

    Exercise72 -> Google Searcher

---
+++ 72.py
@@ -0,0 +1,10 @@
+"""
+Exercise 72
+Question: Create a script that let the user type in a search term and then
+          the program opens the browser and searches the term on Google.
+
+"""
+import webbrowser
+
+query = input("Input your query: ")
+webbrowser.open("https://google.com/search?q=%s" % query)
