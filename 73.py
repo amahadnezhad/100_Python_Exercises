ec9bc7ffd81f16ce32542c102c1f5b6ba64a14c4
Amir <amahadnezhad@gmail.com>
2024-02-06 16:50:08 +0330

    Exercise73 -> Data Multiplier

---
+++ 73.py
@@ -0,0 +1,10 @@
+"""
+Exercise 73
+Question: Create a script that reads this file, multiplies its values by two, and saves the output in a new text file.
+
+"""
+import pandas
+
+data = pandas.read_csv("http://www.pythonhow.com/data/sampledata.txt")
+data_2 = data * 2
+data_2.to_csv("sampledata_x_2.txt", index=None)
