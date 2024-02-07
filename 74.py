e5bfb58a44dd60150576a553e517b0af35cb0db0
Amir <amahadnezhad@gmail.com>
2024-02-07 19:38:22 +0330

    Exercise74 -> File Concatenator

---
+++ 74.py
@@ -0,0 +1,12 @@
+"""
+Exercise 74
+Question: Use pandas  to open the two files as data frames and the concat  method to concatenate the data frames.
+
+"""
+# Answer
+import pandas
+
+data1 = pandas.read_csv("http://www.pythonhow.com/data/sampledata.txt")
+data2 = pandas.read_csv("sampledata_x_2.txt")
+data12 = pandas.concat([data1, data2])
+data12.to_csv("sampledata12.txt", index=None)
