a82ea75ac100b784220929577cf9310665b05d74
amahadnezhad <amahadnezhad@gmail.com>
2024-02-11 16:24:14 +0330

    Exercise84 -> Pyglet

---
+++ 84.py
@@ -0,0 +1,19 @@
+"""
+Exercise 84
+Question:  Write a Hello World Application using pyglet Library.
+
+"""
+import pyglet
+window = pyglet.window.Window()
+label = pyglet.text.Label('Hello, World',
+                          font_name='Times New Roman',
+                          font_size=36,
+                          x=window.width//2, y=window.height//2,
+                          anchor_x='center', anchor_y='center')
+
+@window.event
+def on_draw():
+    window.clear()
+    label.draw()
+
+pyglet.app.run()
